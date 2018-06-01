d3d86e7304b175b9de2a671a496e151712583001
antonkokschool <anton3kok3school@gmail.com>
2018-06-01 10:48:14 +0000

    working on V3

---
+++ order_directorieV2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+source=$1
+dest=$2
+type=$3
+echo "source is $source"
+echo "destination is $dest"
+echo "type is $type"
+if [ "$type" == "maand" ]; then
+	echo 'maand##########################################################'
+	type='%m'
+elif [ "$type" == "week" ]; then
+	echo 'week##########################################################'
+	type='%V'
+else
+	echo 'none##########################################################'
+	exit 1
+fi
+for file in "$source"/*
+do 
+	echo "$file"
+	if [ ! -d  "$dest" ]; then
+		mkdir "$dest"
+		echo "diractory $dest has bin made";
+	else
+		echo "directory $dest already exist";
+	fi
+	date -r "$file" +"%V"
+	cp "$file" "$dest/${file##*/}"
+	echo "$file" + "$dest/${file##*/}"
+	echo 'made file'
+	rm "$file"
+	echo 'removed file'
+done
