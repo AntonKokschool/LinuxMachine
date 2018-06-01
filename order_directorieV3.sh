3d8d8d4bbdbf375f18369df3476a545a30780d56
antonkokschool <anton3kok3school@gmail.com>
2018-06-01 12:17:25 +0000

    Week 2 af + extra parameter

--- order_directorieV3.sh
+++ order_directorieV3.sh
@@ -1,36 +1,45 @@
 #!/bin/bash
 source=$1
 dest=$2
-soort=$3
+type=$3
+dest=$dest
 echo "source is $source"
 echo "destination is $dest"
 echo "type is $type"
-if [ "$soort" == "maand" ]; then
-	echo 'maand##########################################################'
-	type='%m'
-elif [ "$soort" == "week" ]; then
-	echo 'week##########################################################'
-	type='%V'
+if [ "$type" == "maand" ]; then
+	echo 'maand'
+	typedate='%m'
+elif [ "$type" == "week" ]; then
+	echo 'week'
+	typedate='%V'
 else
-	echo 'none##########################################################'
+	echo 'none'
 	exit 1
 fi
 for file in "$source"/*
 do 
-	echo "$file"
+	filedatum=$(date -r $file +$typedate)
 	if [ ! -d  "$dest" ]; then
 		mkdir "$dest"
 		echo "diractory $dest has bin made";
-	else
-		echo "directory $dest already exist";
 	fi
-	filedatum=$(date -r $file +$type)
-	echo "$filedatum is dit111111111111111111111111111111111111111111111111111"
-	#cp "$file" "$dest/$soort$filedatum/${file##*/}"
-	echo "$file" + "$dest/$soort$filedatum/${file##*/}"
-	echo "${file##*/}" #filename  only
-	echo 'made file'
-	#rm "$file"
-	echo 'removed file'
+	if [ ! -d  "$dest/$type/" ]; then
+		mkdir "$dest/$type"
+		echo "diractory $dest/$type has bin made";
+	fi
+	if [ ! -d  "$dest/$type/$filedatum" ]; then
+		mkdir "$dest/$type/$filedatum"
+		echo "diractory $dest/$type/$filedatum has bin made";
+	else
+		echo "directory $dest/$type/$filedatum already exist";
+	fi
+	cp "$file" "$dest/$type/$filedatum/${file##*/}"
+	echo "made file $file" + "$dest/$type/$filedatum/${file##*/}"
+	if [ $(md5sum "$dest/$type/$filedatum/${file##*/}" | cut -d " " -f1) == $(md5sum "$file" | cut -d " " -f1)  ]; then
+		rm "$file"
+		echo 'gelijk en removed file'
+	else
+		echo 'niet gelijk'
+	fi
 	echo '#################################'
 done
